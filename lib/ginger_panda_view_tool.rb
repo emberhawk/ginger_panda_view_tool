2d27c365a5e5ab975d4028ae01aace4788062774
Neil Mckenna <emberhawk97@gmail.com>
2021-01-29 11:32:46 +0000

    Ginger Panda Initial Commit

---
+++ lib/ginger_panda_view_tool.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "ginger_panda_view_tool/version"
+require_relative "ginger_panda_view_tool/renderer"
+
+module GingerPandaViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+
+end
